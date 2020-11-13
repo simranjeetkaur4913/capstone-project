f6843b9896ccfa8c8632410657f6ca728c2a22fb
RjdeepKaur3105 <rajdeep.rkb@gmail.com>
2020-11-13 08:40:44 -0500

    commit by Rajdeep

---
+++ projectcoding/about.php
@@ -0,0 +1,89 @@
+<html>
+    <head>
+        <title>
+            Delta English School --About
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel = "stylesheet" type="text/css" href="about.css">
+        <link rel = "stylesheet" type="text/css" href="main.css">
+
+    </head>
+    <body>
+        <div class="header-part">
+            Get In Touch With Us: +1 2269728372
+        </div>
+        <div class="small-menu-container">
+            <label for="small-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
+        <input type="checkbox" id="small-menu" />
+        <div class="nav">
+            <a href="index.php">Home</a>
+            <a href="about.css">About Us</a>
+            <a href="staff.php">Staff</a>
+            <a href=#>Classes</a>
+            <a href=#>Our Gallery</a>
+            <a href="contact.php">Contact Us</a>
+            <a href=#>Admin Login</a>
+        </div>
+
+
+        <div class = "part0"> About Us </div>    
+    <div class = "part1"> 
+    <h3> WHO WE ARE?</h3>
+    <p class = "P1"> Our school synonymous with the motto of  Enlightenment with education! Within a short span of few years, Delta English School Jalandhar has scaled great heights in every field. The school is thriving with four wings; Pre-Primary, Primary, Senior and Senior Secondary.Presently, the school has on roll above 1900 students who are being nurtured to excel academically, intellectually and spiritually. A variety of co-curricular activities enable the students to develop a healthy global outlook. Morever, our students are being instructed and guided through the latest methods and techniques in the field of science and are information technology savvy, with strong moral and ethical values. The school is a powerful strategic tool to meet the students and parents rising expectations in the challenging knowledge environment.
+    A galaxy of dedicated teachers imparts quality education ensuring all round development of the children. Every year a number of children qualify for admissions in different professional colleges in India and abroad taking with them and upholding the motto of the school “Service Before Self”.</p> </div>
+    <div class = "part2">
+    <marquee> A school synonymous with excellence in education! Within a short span of few years, Our School has scaled great heights in every field. The school is thriving with four wings; Pre-Primary, Primary, Senior and Senior Secondary. The mission of our institution is to enlighten our students; empowering them with high standards of scholastic and co-scholastic excellence; to lead the nurturance of the next decade knowledge, skills, values and attitudes in them to ensure their self-actualization and thereby becoming living examples of our motto “Service Before Self”. </marquee>
+    </div>
+    
+    
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-4 part3" ><h5>Our Mission </h5>The mission of our institution is to enlighten our students; empowering them      with high standards of scholastic and co-scholastic excellence; to lead the nurturance of the next decade knowledge,            skills, values and attitudes in them to ensure their self-actualization and thereby becoming living examples of our motto   “Service Before Self".
+             </div>
+    <div class="col-sm-4 part3"><h5>Our Vision </h5>The mission of our institution is to enlighten our students; empowering them with high standards of scholastic and co-scholastic excellence; to lead the nurturance of the next decade knowledge, skills, values and attitudes in them to ensure their self-actualization and thereby becoming living examples of our motto “Service Before Self".</div>
+    <div class="col-sm-4 part3"><h5>Our Motto </h5>The mission of our institution is to enlighten our students; empowering them with high standards of scholastic and co-scholastic excellence; to lead the nurturance of the next decade knowledge, skills, values and attitudes in them to ensure their self-actualization and thereby becoming living examples of our motto “Service Before Self".</div>
+  </div>
+</div>
+<div class="footer-upper-part">
+<center>Address: 76, KNOTTY PINE AVE, CAMBRIDGE, ONTARIO, CANADA</center>
+</div>
+<div class="container-fluid">
+              <div class="row footer-main">
+                <div class="col-sm-3 footer-inner-1"><h5 style="color:#EBDC87">Who We Are ?</h5>
+                    <p style="font-size:11px;text-align:justify;color:#FAF3DD">Delta Englisgh School is a name you can relia upon. We are proving our services since 2008. we have a dedicated team and every team member is well qualified and experienced in his/her field. We are provding very good result every session and we also get appreciation from parents as well as from other educational bodies. Our school has world class infrastructure. We always organize different type of activities and programs for students which prove very benificial for their overall growth.</p>
+                </div>
+                  
+                <div class="col-sm-3 footer-inner-2"><h5 style="color:#EBDC87">Visit Here:</h5>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>Who We Are?</a></li>
+                        <li><a>Department</a></li>
+                        <li><a>Our Gallery</a></li>
+                        <li><a>Get In Touch With Us</a></li>
+                        <li><a>Know About Our Staff</a></li>
+                        <li><a>Admin Login</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 footer-inner-3"><h5 style="color:#EBDC87">Our Location:</h5>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2899.400050548506!2d-80.40736348508851!3d43.38956717745703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b9018e9e89adf%3A0x2043c24369ede07e!2sConestoga%20College!5e0!3m2!1sen!2sca!4v1603666155898!5m2!1sen!2sca" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+                <div class="col-sm-3 footer-inner-4"><h5 style="color:#EBDC87">Who We Are ?</h5></div>
+              </div>
+        </div>
+        <div class="footer-lower-part">
+            <center>Created By: Conestoga College Students</center>
+        </div>
+
+    </body>
+
+</html>
+
