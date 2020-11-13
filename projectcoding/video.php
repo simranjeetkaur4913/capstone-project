9cc4531596e0dd53e1d5033c34bcd783031a7c80
Gurmeet-Singh1988 <connections.gurmeet@gmail.com>
2020-11-12 23:19:34 -0500

    Commit By Gurmeet Singh

---
+++ projectcoding/video.php
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <title>Delta English School</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="header-part">
+            Get In Touch With Us: +1 2269728272
+        </div>
+        <div class="small-menu-container">
+            <label for="small-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
+        <input type="checkbox" id="small-menu" />
+        <div class="nav">
+            <a href="index.php">Home</a>
+            <a href=#>About Us</a>
+            <a href=#>Staff</a>
+            <a href=#>Classes</a>
+            <a href="video.php">Our Gallery</a>
+            <a href="contact.php">Contact Us</a>
+            <a href=#>Admin Login</a>
+        </div>
+        <div class="contact-top">Video Gallery</div>
+        <div class="container-fluid">
+              <div class="row">
+                <div class="col-sm-6 videoholder">
+                    <iframe width="100%" height="300" src="https://www.youtube.com/embed/Efp6J1WyrVI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="col-sm-6 videoholder">
+                     <iframe width="100%" height="300" src="https://www.youtube.com/embed/Efp6J1WyrVI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+              </div>
+        </div>
+         <div class="container-fluid">
+              <div class="row">
+                <div class="col-sm-6 videoholder">
+                    <iframe width="100%" height="300" src="https://www.youtube.com/embed/Efp6J1WyrVI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="col-sm-6 videoholder">
+                     <iframe width="100%" height="300" src="https://www.youtube.com/embed/Efp6J1WyrVI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+              </div>
+        </div>
+        <div class="footer-upper-part">
+            <center>Address: 76, KNOTTY PINE AVE, CAMBRIDGE, ONTARIO, CANADA</center>
+        </div>
+        <div class="container-fluid">
+              <div class="row footer-main">
+                <div class="col-sm-3 footer-inner-1"><h5 style="color:#EBDC87">Who We Are ?</h5>
+                    <p style="font-size:11px;text-align:justify;color:#FAF3DD">Delta Englisgh School is a name you can relia upon. We are proving our services since 2008. we have a dedicated team and every team member is well qualified and experienced in his/her field. We are provding very good result every session and we also get appreciation from parents as well as from other educational bodies. Our school has world class infrastructure. We always organize different type of activities and programs for students which prove very benificial for their overall growth.</p>
+                </div>
+                <div class="col-sm-3 footer-inner-2"><h5 style="color:#EBDC87">Visit Here:</h5>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>Who We Are?</a></li>
+                        <li><a>Department</a></li>
+                        <li><a>Our Gallery</a></li>
+                        <li><a>Get In Touch With Us</a></li>
+                        <li><a>Know About Our Staff</a></li>
+                        <li><a>Admin Login</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3 footer-inner-3"><h5 style="color:#EBDC87">Our Location:</h5>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2899.400050548506!2d-80.40736348508851!3d43.38956717745703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b9018e9e89adf%3A0x2043c24369ede07e!2sConestoga%20College!5e0!3m2!1sen!2sca!4v1603666155898!5m2!1sen!2sca" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+                <div class="col-sm-3 footer-inner-4"><h5 style="color:#EBDC87">Who We Are ?</h5></div>
+              </div>
+        </div>
+        <div class="footer-lower-part">
+            <center>Created By: Conestoga College Students</center>
+        </div>
+    </body>
+</html>
