db0a599e271acd0f53987b809dea48dcdbae59b0
simranjeetkaur4913 <simranjeetkaur4913@gmail.com>
2020-12-18 01:41:23 -0500

    done by simranjeet kaur

--- projectcoding/welcome-admin.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title>Admin Panal</title>
-    </head>
-    <body style="background-color:#EBDC87">
-        <h2 style="text-align:center">Welcome To Admin Panal</h2>
-    </body>
-</html>
