b6ff292dc8753faf59fda2b77a5db267ae6b3f40
RjdeepKaur3105 <rajdeep.rkb@gmail.com>
2020-12-18 02:34:39 -0500

    by Rajdeep

--- delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('C:\xampp\htdocs\capstoneproject\projectcoding\php\mysqli_connect.php');
-$id = $_GET['id'];
-$q = "DELETE FROM staff WHERE id = '$id'";
-mysqli_query($dbc,$q);
-header('location:staff-administration.php');
-?>
